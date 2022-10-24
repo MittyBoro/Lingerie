263339efec0fc5ef797906381d63596f1a43755a
Dima Boro <iboro770@gmail.com>
2022-10-24 12:51:54 +0300

    page & faq html

--- resources/front/views/pages/faq.blade.php
+++ resources/front/views/pages/faq.blade.php
@@ -6,9 +6,35 @@
 
 @section('content')
 
-<div class="home-screen">
+<div class="faq-box">
     <div class="container">
-
+        <h1 class="h2">частые вопросы</h1>
+        <div class="faq-list">
+            <div class="faq-item">
+                <div class="faq-title">Могу ли я оформить заказ по телефону?</div>
+                <div class="faq-value">Всdddddddd</div>
+            </div>
+            <div class="faq-item active">
+                <div class="faq-title">Я хочу сделать подарок. Как будет выглядит упаковка заказа?</div>
+                <div class="faq-value">Все заказы, сделанные в интернет-магазине, бережно упаковываются в наш розовую брендированную коробку, перевязанную черной атласной ленточкой. Перед отправкой заказа мы помещаем розовую коробку в крафтовый короб. На внешней упаковке указан только адрес получателя и не указан отправитель заказа и его содержимое, что обеспечивает полную анонимность.</div>
+            </div>
+            <div class="faq-item">
+                <div class="faq-title">Сколько вещей я могу оформить в одном заказе?</div>
+                <div class="faq-value">Всdddddddd</div>
+            </div>
+            <div class="faq-item">
+                <div class="faq-title">Я сомневаюсь в выборе размера товара. Как мне быть?</div>
+                <div class="faq-value">Всdddddddd</div>
+            </div>
+            <div class="faq-item">
+                <div class="faq-title">Как быстро доставят мой заказ?</div>
+                <div class="faq-value">Всdddddddd</div>
+            </div>
+            <div class="faq-item">
+                <div class="faq-title">Что делать, если нужного мне товара нет в наличии?</div>
+                <div class="faq-value">Всdddddddd</div>
+            </div>
+        </div>
     </div>
 </div>
 
