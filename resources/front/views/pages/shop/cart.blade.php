1f03cd3af86f1c8e034a79a642b36751bb2ede9a
Dima Boro <iboro770@gmail.com>
2022-10-24 12:28:34 +0300

    cart html

--- resources/front/views/pages/shop/cart.blade.php
+++ resources/front/views/pages/shop/cart.blade.php
@@ -6,9 +6,36 @@
 
 @section('content')
 
-<div class="home-screen">
+<div class="cart-box">
     <div class="container">
-
+        <div class="h2">ваша корзина</div>
+        <div class="cart-list">
+            @foreach (range(1, 3) as $i)
+                <div class="cart-item">
+                    <div class="cart-image-col">
+                        <img src="/storage/tmp/1.png" alt="">
+                    </div>
+                    <div class="cart-name-col">
+                        <div class="cart-name">Длинный заголовок чудесного товара</div>
+                        <div class="cart-attr"><span>Размер: XL</span></div>
+                    </div>
+                    <div class="cart-count">
+                        <span class="circle">-</span>
+                        <span class="int">1</span>
+                        <span class="circle">+</span>
+                    </div>
+                    <div class="cart-price-col">
+                        <div class="cart-price"><span>6900</span> ₽</div>
+                        <div class="cart-remove">Удалить</div>
+                    </div>
+                </div>
+            @endforeach
+            <div class="cart-item cart-subtotal-row">
+                <div class="cart-sb-name">Подытог</div>
+                <div class="cart-price"><span>17 790</span> ₽</div>
+                <div class="btn">оформить заказ</div>
+            </div>
+        </div>
     </div>
 </div>
 
