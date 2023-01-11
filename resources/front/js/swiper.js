3fbe81702f5ab39b6b47d4b9cce9892cb9b8e094
Dima Boro <iboro770@gmail.com>
2023-01-11 21:45:49 +0300

    product first row

--- resources/front/js/swiper.js
+++ resources/front/js/swiper.js
@@ -3,27 +3,28 @@
 import Swiper, {
     Navigation,
     Autoplay ,
-    // Pagination,
-    // Thumbs,
+    Thumbs,
     Keyboard,
     EffectFade,
+    Scrollbar,
+    Mousewheel,
 } from 'swiper'
-
-import 'swiper/css';
-import 'swiper/css/effect-fade';
-// import 'swiper/css/navigation';
-// import 'swiper/css/autoplay';
-
 
 Swiper.use([
     Navigation,
     Autoplay ,
-    // Pagination,
-    // Thumbs,
+    Thumbs,
     Keyboard,
     EffectFade,
+    Scrollbar,
+    Mousewheel,
 ]);
 
+
+import 'swiper/css';
+import 'swiper/css/effect-fade';
+import 'swiper/css/scrollbar';
+// import 'swiper/css/autoplay';
 
 
 // слайдер home ПОПУЛЯРНЫЕ НОВИНКИ
@@ -88,8 +89,9 @@ new Swiper('.home-novelties-box .swiper', {
 });
 
 
+
+// слайдер home ПОПУЛЯРНЫЕ НОВИНКИ
 document.querySelectorAll('.catalog-item .swiper').forEach(el => {
-    // слайдер home ПОПУЛЯРНЫЕ НОВИНКИ
 
     let parent = el.closest('.catalog-item')
     new Swiper(el, {
@@ -125,3 +127,80 @@ document.querySelectorAll('.catalog-item .swiper').forEach(el => {
         // }
     });
 })
+
+
+
+// product-gallery
+document.querySelectorAll('.product-box .gallery-col').forEach(el => {
+
+    // слайдер товары
+    let thumbContainer = el.querySelector('.thumbs-row .swiper');
+
+    let galleryThumbs = new Swiper(thumbContainer, {
+        spaceBetween: 10,
+        slidesPerView: 'auto',
+        direction: 'vertical',
+        mousewheel: true,
+        scrollbar: {
+            el: ".swiper-scrollbar",
+            hide: false,
+            draggable: true,
+        },
+
+        on: {
+            init: (swiper) => {
+                if (swiper.slides.length < 2) {
+                    el.querySelector('.full-row').classList.add('hidden');
+                }
+            },
+        },
+        breakpoints: {
+            576: {
+            },
+            768: {
+            },
+            992: {
+                spaceBetween: 25,
+                slidesPerView: 4,
+            },
+            1240: {
+            }
+        }
+    });
+
+
+    let topContainer = el.querySelector('.full-row .swiper')
+
+    new Swiper(topContainer, {
+        spaceBetween: 15,
+        loop: true,
+        effect: 'fade',
+        navigation: {
+            nextEl: el.querySelector('.sw-next'),
+            prevEl: el.querySelector('.sw-prev'),
+        },
+        thumbs: {
+            swiper: galleryThumbs,
+        },
+    });
+
+    if (topContainer) {
+        topContainer.querySelectorAll('.zoom-image').forEach(el => {
+            el.addEventListener('mousemove', zoom)
+        })
+    }
+});
+
+
+function zoom(e){
+    let zoomer = e.currentTarget;
+	let offsetX, offsetY;
+
+    e.offsetX ? (offsetX = e.offsetX) : (offsetX = e.touches[0].pageX);
+    e.offsetY ? (offsetY = e.offsetY) : (offsetX = e.touches[0].pageX);
+
+    let x = offsetX / zoomer.offsetWidth  * 100;
+    let y = offsetY / zoomer.offsetHeight * 100;
+
+    zoomer.style.backgroundPosition = x + "% " + y +"%";
+}
--- resources/views/pages/shop/product.blade.php
+++ resources/views/pages/shop/product.blade.php
@@ -8,87 +8,131 @@
 
 <div class="product-box">
     <div class="container">
-        <div class="top-info-col">
-            <div class="breadcrumbs">
-                <a href="#">Главная</a>
-                <span class="delimeter">/</span>
-                <a href="#">Нижнее белье</a>
-                <span class="delimeter">/</span>
-                <a href="#">Пояса</a>
-            </div>
-            <h2 class="h2">Длинный заголовок чудесного товара</h2>
+        <div class="prod-grid grid-12">
+            <div class="prod-main-col">
+                <div class="breadcrumbs">
+                    <a href="#">Главная</a>
+                    <span class="delimeter">/</span>
+                    <a href="#">Нижнее белье</a>
+                    <span class="delimeter">/</span>
+                    <a href="#">Пояса</a>
+                </div>
+                <div class="prod-title"><h1>Длинный заголовок чудесного товара</h1></div>
 
-            <div class="prod-attr-wrap">
-                <div class="pa-title">Размер</div>
-                <div class="pa-list pa-size">
-                    <div class="pa-item">
-                        <input type="radio" name="size">
-                        <span>XS</span>
+                <div class="prod-attr-wrap">
+                    <div class="pm-title">Размер</div>
+                    <div class="pm-list pm-size">
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="size">
+                            <div class="fr-item">
+                                <span>XS</span>
+                            </div>
+                        </label>
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="size">
+                            <div class="fr-item">
+                                <span>S</span>
+                            </div>
+                        </label>
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="size">
+                            <div class="fr-item">
+                                <span>M</span>
+                            </div>
+                        </label>
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="size">
+                            <div class="fr-item">
+                                <span>L</span>
+                            </div>
+                        </label>
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="size">
+                            <div class="fr-item">
+                                <span>XL</span>
+                            </div>
+                        </label>
                     </div>
-                    <div class="pa-item">
-                        <input type="radio" name="size">
-                        <span>S</span>
+                </div>
+                <div class="prod-attr-wrap">
+                    <div class="pm-title">Цвет</div>
+                    <div class="pm-list pm-color">
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="color" value="1">
+                            <div class="fr-item">
+                                <span style="background-color: #fff; border: 1px solid #D9D9D9;"></span>
+                            </div>
+                        </label>
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="color" value="2">
+                            <div class="fr-item">
+                                <span style="background-color: #F67FC6"></span>
+                            </div>
+                        </label>
+                        <label class="form-radio pm-item">
+                            <input type="radio" name="color" value="3">
+                            <div class="fr-item">
+                                <span style="background-color: #88DD61"></span>
+                            </div>
+                        </label>
                     </div>
-                    <div class="pa-item">
-                        <input type="radio" name="size">
-                        <span>M</span>
+                </div>
+                <div class="prod-price">4990 ₽</div>
+                <div class="btn">добавить в корзину</div>
+            </div>
+
+            <div class="gallery-col">
+                <div class="full-row">
+                    <div class="swiper">
+                        <div class="swiper-wrapper">
+                            @foreach ([1, 2, 1, 2, 1, 2] as $i)
+                                <div class="swiper-slide">
+                                    <div class="gallery-image zoom-image" style="background-image: url('/storage/tmp/{{ $i }}.png')"><img src="/storage/tmp/{{ $i }}.png" alt=""></div>
+                                </div>
+                            @endforeach
+                        </div>
                     </div>
-                    <div class="pa-item">
-                        <input type="radio" name="size">
-                        <span>L</span>
+                    <div class="sw-arrow sw-prev">
+                        <img src="@vite_asset('images/icons/arrow-line-right.svg')" alt="" class="to-svg">
                     </div>
-                    <div class="pa-item">
-                        <input type="radio" name="size">
-                        <span>XL</span>
+                    <div class="sw-arrow sw-next">
+                        <img src="@vite_asset('images/icons/arrow-line-right.svg')" alt="" class="to-svg">
+                    </div>
+                </div>
+                <div class="thumbs-row">
+                    <div class="swiper-scrollbar"></div>
+                    <div class="swiper-container">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                @foreach ([1, 2, 1, 2, 1, 2] as $i)
+                                    <div class="swiper-slide">
+                                        <div class="gallery-image"><img src="/storage/tmp/{{ $i }}.png" alt=""></div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="prod-attr-wrap">
-                <div class="pa-title">Цвет</div>
-                <div class="pa-list pa-color">
-                    <div class="pa-item">
-                        <input type="radio" name="color">
-                        <span style="background-color: #fff"></span>
-                    </div>
-                    <div class="pa-item">
-                        <input type="radio" name="color">
-                        <span style="background-color: #F67FC6"></span>
-                    </div>
-                    <div class="pa-item">
-                        <input type="radio" name="color">
-                        <span style="background-color: #88DD61"></span>
-                    </div>
+
+            {{-- <div class="bottom-info-col">
+                <div class="bi-item">
+                    <div class="b-title">Описание</div>
+                    <div class="bi-text">Этот пояс для чулок из лаконичной и в то же время чувственной коллекции Henrieta выполнен из розового тюля. Передняя панель украшена вышивкой с растительным мотивом. Края обрамлены эластичными лентами. </div>
+                </div>
+                <div class="bi-item">
+                    <div class="b-title">Состав</div>
+                    <div class="bi-text">Верхний слой с вышивкой: Основа: 100% полиамид, Вышивка: 100% полиэстер, Сетчатый верхний слой: 100% полиамид. </div>
+                </div>
+                <div class="bi-item">
+                    <div class="b-title">уход</div>
+                    <div class="bi-text">Рекомендована раздельная ручная стирка и бережная сушка.</div>
                 </div>
             </div>
-            <div class="prod-price">4990 ₽</div>
-            <div class="btn">добавить в корзину</div>
-        </div>
-        <div class="gallery-col">
-            <div class="big-images-row">
-                <img src="/storage/tmp/1.png" alt="">
-            </div>
-            <div class="thumbs-row">
-                <img src="/storage/tmp/1.png" alt="">
-                <img src="/storage/tmp/2.png" alt="">
-            </div>
-        </div>
-        <div class="bottom-info-col">
-            <div class="bi-item">
-                <div class="b-title">Описание</div>
-                <div class="bi-text">Этот пояс для чулок из лаконичной и в то же время чувственной коллекции Henrieta выполнен из розового тюля. Передняя панель украшена вышивкой с растительным мотивом. Края обрамлены эластичными лентами. </div>
-            </div>
-            <div class="bi-item">
-                <div class="b-title">Состав</div>
-                <div class="bi-text">Верхний слой с вышивкой: Основа: 100% полиамид, Вышивка: 100% полиэстер, Сетчатый верхний слой: 100% полиамид. </div>
-            </div>
-            <div class="bi-item">
-                <div class="b-title">уход</div>
-                <div class="bi-text">Рекомендована раздельная ручная стирка и бережная сушка.</div>
-            </div>
-        </div>
-        <div class="bottom-table-col">
-            <div class="b-title">таблица размеров</div>
-            <img src="/storage/tmp/table.png" alt="">
+            <div class="bottom-table-col">
+                <div class="b-title">таблица размеров</div>
+                <img src="/storage/tmp/table.png" alt="">
+            </div> --}}
         </div>
     </div>
 </div>
@@ -97,13 +141,27 @@
     <div class="container">
         <div class="handwritten">Look</div>
         <div class="h2">вам может понравиться</div>
-        <div class="catalog-list">
-            @foreach (range(0, 3) as $i)
+        <div class="catalog-list grid-4">
+            @foreach (range(1, 4) as $i)
                 <div class="catalog-item">
-                    <div class="catalog-images">
-                        <img src="/storage/tmp/1.png" alt="">
+                    <div class="catalog-images-wrapper">
+                        <div class="swiper">
+                            <div class="swiper-wrapper">
+                                @foreach (range(1, 3) as $i)
+                                    <div class="swiper-slide">
+                                        <a href="#" class="catalog-image"><img src="/storage/tmp/{{ rand(1, 2) }}.png" alt=""></a>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                        <div class="sw-arrow sw-prev">
+                            <img src="@vite_asset('images/icons/arrow-line-right.svg')" alt="" class="to-svg">
+                        </div>
+                        <div class="sw-arrow sw-next">
+                            <img src="@vite_asset('images/icons/arrow-line-right.svg')" alt="" class="to-svg">
+                        </div>
                     </div>
-                    <a href="#">Длинный заголовок чудесного товара</a>
+                    <a href="#" class="ci-name">Длинный заголовок чудесного товара</a>
                     <div class="ci-price"><span>4990</span> ₽</div>
                 </div>
             @endforeach
