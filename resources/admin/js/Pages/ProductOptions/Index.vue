f8903f2c94103f9bca71052df2630810975aea47
MittyBoro <iboro770@gmail.com>
2023-12-11 16:33:37 +0300

    options colors...

--- resources/admin/js/Pages/ProductOptions/Index.vue
+++ resources/admin/js/Pages/ProductOptions/Index.vue
@@ -16,11 +16,11 @@
                             <div class="-mx-2 flex gap-3">
                                 <input v-if="sp.element.type == 'color'" type="color" class="w-6 h-7 flex-shrink-0 rounded-full cursor-pointer" v-model="sp.element.extra"
                                     @change="update(sp.element)">
-                                <FTextarea v-else
+                                <FTextarea
                                     rows="1"
                                     class="max-h-16 focus:max-h-max"
-                                    @change="update(sp.element)"
-                                    v-model="sp.element.extra"
+                                    @update:modelValue="sp.element.extra = $event;update(sp.element)"
+                                    v-model.lazy="sp.element.extra"
                                 />
                             </div>
                         </TData>
